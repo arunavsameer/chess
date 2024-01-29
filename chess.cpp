8581618fa963ae1335bd848cd3a73ad50aa801b1
Arunav Sameer <97653048+arunavsameer@users.noreply.github.com>
2024-01-30 01:03:35 +0530

    Update chess.cpp

--- chess.cpp
+++ chess.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#include <fstream>
 using namespace std;
 
 string B1 = "R1K1B1KGQNB2K2R2";
@@ -10,19 +11,21 @@ string spacer = "|----+----+----+----+----+----+----+----|";
 string spacer_ends = "-----------------------------------------";
 
 // TO-DO
-// add movement by input parsing
-// add algorithm for each type of piece
-// add history.txt and create history for games... also try to somehow read history and recreate the game's last move
+// add algorithm for each type of piece and moves like castling
+// read history and recreate the game's last move
 // try to make the black and white box pattern, or somehow immitate it
-// create print_board for white side as well
 
 class game
 {
     string board[8][8];
+    ofstream history;
 
 public:
+    bool game_over = false;
+
     void reset_board()
     {
+
         for (int i = 0; i < 8; i++)
         {
             board[0][i] += B1[(2 * i)];
@@ -52,52 +55,146 @@ public:
         }
     }
 
-    void input_black()
+    void input(int n)
     {
-    }
-
-    void input_white()
-    {
-    }
-
-    void algorithm()
-    {
-    }
-
-    void print_board()
-    {
-        cout << " ";
-        for (int i = 0; i < 8; i++)
+        int delta;
+        if (n % 2 == 0)
         {
-            cout << "    " << column[i];
+            delta = 97;
         }
-        cout << endl;
-        string spacer_local;
-        for (int i = 0; i < 8; i++)
+        else
         {
-            if (i == 0)
-            {
-                spacer_local = spacer_ends;
-            }
-            else
-            {
-                spacer_local = spacer;
-            }
-            cout << "  " << spacer_local << endl
-                 << i + 1;
-            for (int j = 0; j < 8; j++)
-            {
-                cout << " | " << board[i][j];
-            }
-            cout << " |" << endl;
+            delta = 65;
         }
-        cout << "  " << spacer_ends << endl;
+
+        string input;
+        bool valid = false;
+        bool occur = false;
+        pair<int, int> current;
+        pair<int, int> final;
+        string piece;
+
+        do
+        {
+            cin >> input;
+            piece += input[0];
+            piece += input[1];
+            if (input[0] >= delta)
+            {
+                for (int i = 0; i < 8; i++)
+                {
+                    for (int j = 0; j < 8; j++)
+                    {
+                        if (board[i][j] == piece)
+                        {
+                            occur = true;
+                            current.first = i;
+                            current.second = j;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            final.first = input[3] - 49; // input[3] will be as char and not int data type
+            final.second = input[2] - 65;
+
+            if (occur)
+            {
+                if (algorithm())
+                {
+                    valid = true;
+                    history.open("history.txt", std::ios_base::app);
+                    if (history.is_open())
+                    {
+                        history << input << endl;
+                    }
+                    history.close();
+                }
+            }
+        } while (!valid);
+
+        board[final.first][final.second] = board[current.first][current.second];
+        board[current.first][current.second] = "  ";
+    }
+
+    bool algorithm()
+    {
+        return true; // test
+    }
+
+    void print_board(int n)
+    {
+        if (n % 2 == 0)
+        {
+            cout << " ";
+            for (int i = 0; i < 8; i++)
+            {
+                cout << "    " << column[i];
+            }
+            cout << endl;
+            string spacer_local;
+            for (int i = 0; i < 8; i++)
+            {
+                if (i == 0)
+                {
+                    spacer_local = spacer_ends;
+                }
+                else
+                {
+                    spacer_local = spacer;
+                }
+                cout << "  " << spacer_local << endl
+                     << i + 1;
+                for (int j = 0; j < 8; j++)
+                {
+                    cout << " | " << board[i][j];
+                }
+                cout << " |" << endl;
+            }
+            cout << "  " << spacer_ends << endl;
+        }
+        else
+        {
+            cout << " ";
+            for (int i = 7; i >= 0; i--)
+            {
+                cout << "    " << column[i];
+            }
+            cout << endl;
+            string spacer_local;
+            for (int i = 7; i >= 0; i--)
+            {
+                if (i == 7)
+                {
+                    spacer_local = spacer_ends;
+                }
+                else
+                {
+                    spacer_local = spacer;
+                }
+                cout << "  " << spacer_local << endl
+                     << i + 1;
+                for (int j = 7; j >= 0; j--)
+                {
+                    cout << " | " << board[i][j];
+                }
+                cout << " |" << endl;
+            }
+            cout << "  " << spacer_ends << endl;
+        }
     }
 };
 
 int main()
 {
     game g;
+    int moves = 0;
     g.reset_board();
-    g.print_board();
+    while (!g.game_over)
+    {
+        g.print_board(moves);
+        g.input(moves);
+        moves++;
+    }
 }
